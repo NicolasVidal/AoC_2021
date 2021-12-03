ea76c34770318af953dffc8fcb5ac82c3e3e2539
NicolasVidal-IsCool <nicolas.vidal@iscool-e.Com>
2021-12-03 18:49:48 +0100

    Add Criterion to be able to run performance benchmarks on solutions

---
+++ src/bench_j2.rs
@@ -0,0 +1,12 @@
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
+
+mod j2;
+
+
+fn bench(c: &mut Criterion) {
+    c.bench_function("bench_j2_p1", |b| b.iter(|| j2::_p1(black_box(include_str!("j2/j2.txt")))));
+    c.bench_function("bench_j2_p2", |b| b.iter(|| j2::_p2(black_box(include_str!("j2/j2.txt")))));
+}
+
+criterion_group!(benches, bench);
+criterion_main!(benches);
---
+++ src/bench_j3.rs
@@ -0,0 +1,12 @@
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
+
+mod j3;
+
+
+fn bench(c: &mut Criterion) {
+    c.bench_function("bench_j3_p1", |b| b.iter(|| j3::_p1(black_box(include_str!("j3/j3.txt")))));
+    c.bench_function("bench_j3_p2", |b| b.iter(|| j3::_p2(black_box(include_str!("j3/j3.txt")))));
+}
+
+criterion_group!(benches, bench);
+criterion_main!(benches);
