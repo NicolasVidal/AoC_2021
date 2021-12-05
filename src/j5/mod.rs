733e7df46769eaf5fbc2b3cd653a01a83a6395a4
NicolasVidal-IsCool <nicolas.vidal@iscool-e.Com>
2021-12-05 10:20:23 +0100

    j5 - a bit factorized

--- src/j5/mod.rs
+++ src/j5/mod.rs
@@ -7,33 +7,56 @@ fn parse_group(s: &str) -> (usize, usize) {
      usize::from_str(splitted.next().unwrap()).unwrap())
 }
 
+fn parse_and_extract_move_info_from_line(line: &str) -> (usize, usize, i32, i32, i32, i32) {
+    let mut groups = line.split_whitespace();
+    let (x1, y1) = parse_group(groups.next().unwrap());
+    let _ = groups.next().unwrap();
+    let (x2, y2) = parse_group(groups.next().unwrap());
+
+    let diff_x = x2 as i32 - x1 as i32;
+    let step_diff_x = diff_x.signum();
+    let abs_diff_x = diff_x.abs();
+    let diff_y = y2 as i32 - y1 as i32;
+    let step_diff_y = diff_y.signum();
+    let abs_diff_y = diff_y.abs();
+    (x1, y1, step_diff_x, abs_diff_x, step_diff_y, abs_diff_y)
+}
+
+fn update_cells(covered_positions: &mut HashMap<(usize, usize), i32>,
+                at_least_two_overlaps: &mut usize,
+                x1: usize,
+                y1: usize,
+                step_diff_x: i32,
+                abs_diff_x: i32,
+                step_diff_y: i32,
+                abs_diff_y: i32) {
+    let (mut x, mut y) = (x1 as i32, y1 as i32);
+    for _ in 0..=abs_diff_x.max(abs_diff_y) {
+        let v = covered_positions.entry((x as usize, y as usize)).or_insert(0);
+        *v += 1;
+        if *v == 2 {
+            *at_least_two_overlaps += 1;
+        }
+        x += step_diff_x;
+        y += step_diff_y;
+    }
+}
+
 pub fn _p1(s: &str) -> usize {
     let mut covered_positions = HashMap::new();
     let mut at_least_two_overlaps = 0;
     for line in s.lines() {
-        let mut groups = line.split_whitespace();
-        let (x1, y1) = parse_group(groups.next().unwrap());
-        let _ = groups.next().unwrap();
-        let (x2, y2) = parse_group(groups.next().unwrap());
-
-        if x1 == x2 {
-            for i in if y1 <= y2 { y1..=y2 } else { y2..=y1 } {
-                let v = covered_positions.entry((x1, i)).or_insert(0);
-                *v += 1;
-                if *v == 2 {
-                    at_least_two_overlaps += 1;
-                }
-            }
-        }
-
-        if y1 == y2 {
-            for i in if x1 <= x2 { x1..=x2 } else { x2..=x1 } {
-                let v = covered_positions.entry((i, y1)).or_insert(0);
-                *v += 1;
-                if *v == 2 {
-                    at_least_two_overlaps += 1;
-                }
-            }
+        let (x1, y1, step_diff_x, abs_diff_x, step_diff_y, abs_diff_y) =
+            parse_and_extract_move_info_from_line(line);
+        if abs_diff_x == 0 || abs_diff_y == 0 {
+            update_cells(&mut covered_positions,
+                         &mut at_least_two_overlaps,
+                         x1,
+                         y1,
+                         step_diff_x,
+                         abs_diff_x,
+                         step_diff_y,
+                         abs_diff_y)
         }
     }
 
@@ -48,61 +71,20 @@ pub fn _p2(s: &str) -> usize {
     let mut covered_positions = HashMap::new();
     let mut at_least_two_overlaps = 0;
     for line in s.lines() {
-        let mut groups = line.split_whitespace();
-        let (x1, y1) = parse_group(groups.next().unwrap());
-        let _ = groups.next().unwrap();
-        let (x2, y2) = parse_group(groups.next().unwrap());
+        let (x1, y1, step_diff_x, abs_diff_x, step_diff_y, abs_diff_y) =
+            parse_and_extract_move_info_from_line(line);
 
-        if x1 == x2 {
-            for i in if y1 <= y2 { y1..=y2 } else { y2..=y1 } {
-                let v = covered_positions.entry((x1, i)).or_insert(0);
-                *v += 1;
-                if *v == 2 {
-                    at_least_two_overlaps += 1;
-                }
-            }
-        }
-
-        if y1 == y2 {
-            for i in if x1 <= x2 { x1..=x2 } else { x2..=x1 } {
-                let v = covered_positions.entry((i, y1)).or_insert(0);
-                *v += 1;
-                if *v == 2 {
-                    at_least_two_overlaps += 1;
-                }
-            }
-        }
-
-        let diff_x = x2 as i32 - x1 as i32;
-        let step_diff_x = diff_x.signum();
-        let abs_diff_x = diff_x.abs();
-        let diff_y = y2 as i32 - y1 as i32;
-        let step_diff_y = diff_y.signum();
-        let abs_diff_y = diff_y.abs();
-        if abs_diff_x == abs_diff_y {
-            let (mut x, mut y) = (x1 as i32, y1 as i32);
-            for _ in 0..=abs_diff_x {
-                let v = covered_positions.entry((x as usize, y as usize)).or_insert(0);
-                *v += 1;
-                if *v == 2 {
-                    at_least_two_overlaps += 1;
-                }
-                x += step_diff_x;
-                y += step_diff_y;
-            }
+        if abs_diff_x == abs_diff_y || abs_diff_x == 0 || abs_diff_y == 0 {
+            update_cells(&mut covered_positions,
+                         &mut at_least_two_overlaps,
+                         x1,
+                         y1,
+                         step_diff_x,
+                         abs_diff_x,
+                         step_diff_y,
+                         abs_diff_y)
         }
     }
-
-    // for y in 0..10 {
-    //     for x in 0..10 {
-    //         let ent = covered_positions.entry((x, y)).or_insert(0);
-    //         print!("{}", match *ent {
-    //             0 => format!("."),
-    //             i => format!("{}", i)
-    //         })
-    //     }
-    //     println!();
-    // }
 
     at_least_two_overlaps
 }
