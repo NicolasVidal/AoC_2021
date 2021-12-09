6d2aa50e130a1f24a4f31bf03440ac3a03e4a08a
NicolasVidal-IsCool <nicolas.vidal@iscool-e.Com>
2021-12-10 00:06:02 +0100

    j9 : even better performances (-71% for p2)

--- src/j9/mod.rs
+++ src/j9/mod.rs
@@ -2,14 +2,19 @@ use std::collections::HashMap;
 
 use itertools::Itertools;
 
-fn compute_matrix_height_width_lower_points_risks(s: &str) -> (Vec<Vec<u8>>, usize, usize, Vec<(usize, usize)>, usize) {
+fn compute_matrix_height_width(s: &str) -> (Vec<Vec<u8>>, usize, usize) {
     let matrix = s.lines().into_iter().map(|line|
         line.chars().into_iter().map(|c| c as u8 - 48u8).collect_vec()
     ).collect_vec();
     let height = matrix.len();
     let width = matrix[0].len();
 
-    let mut lower_points = Vec::new();
+    (matrix, height, width)
+}
+
+pub fn _p1(s: &str) -> usize {
+    let (matrix, height, width) = compute_matrix_height_width(s);
+
     let mut risks = 0usize;
     for i in 0usize..height {
         for j in 0usize..width {
@@ -29,17 +34,11 @@ fn compute_matrix_height_width_lower_points_risks(s: &str) -> (Vec<Vec<u8>>, usi
             }
 
             if matrix[i][j] < min {
-                lower_points.push((i, j));
                 risks += (matrix[i][j] as usize) + 1;
             }
         }
     }
-
-    (matrix, height, width, lower_points, risks)
-}
-
-pub fn _p1(s: &str) -> usize {
-    compute_matrix_height_width_lower_points_risks(s).4
+    risks
 }
 
 pub fn p1() -> usize {
@@ -47,14 +46,13 @@ pub fn p1() -> usize {
 }
 
 pub fn _p2(s: &str) -> usize {
-    let (matrix, height, width, lower_points, _) =
-        compute_matrix_height_width_lower_points_risks(s);
+    let (matrix, height, width) = compute_matrix_height_width(s);
 
-    let mut basin_sizes = lower_points.iter().map(|_| 1usize).collect_vec();
+    let mut basin_sizes = HashMap::new();
     let mut low_point_basins = HashMap::new();
 
     for i in 0..height {
-        'inner: for j in 0..width {
+        for j in 0..width {
             if low_point_basins.contains_key(&(i, j)) {
                 continue;
             }
@@ -63,12 +61,12 @@ pub fn _p2(s: &str) -> usize {
 
             let (mut row, mut col) = (i, j);
 
-            while !lower_points.contains(&(row, col)) {
+            loop {
                 if let Some(&(row, col)) = low_point_basins.get(&(row, col)) {
                     for p in path {
                         low_point_basins.insert(p, (row, col));
                     }
-                    continue 'inner;
+                    break;
                 }
 
                 if matrix[row][col] < 9 {
@@ -76,7 +74,7 @@ pub fn _p2(s: &str) -> usize {
                 }
 
                 let mut lowest = (row, col);
-                let mut lowest_score = u8::MAX;
+                let mut lowest_score = matrix[row][col];
                 if row > 0 && matrix[row - 1][col] < lowest_score {
                     lowest = (row - 1, col);
                     lowest_score = matrix[row - 1][col];
@@ -92,27 +90,29 @@ pub fn _p2(s: &str) -> usize {
                 if col < width - 1 && matrix[row][col + 1] < lowest_score {
                     lowest = (row, col + 1);
                 }
+
+                if (row, col) == lowest {
+                    for p in path {
+                        low_point_basins.insert(p, (row, col));
+                    }
+                    break;
+                }
+
                 row = lowest.0;
                 col = lowest.1;
             }
-
-            for p in path {
-                low_point_basins.insert(p, (row, col));
-            }
         }
     }
 
-    for (idx, &(row, col)) in lower_points.iter().enumerate() {
-        for (_, &(a, b)) in low_point_basins.iter() {
-            if row == a && col == b {
-                basin_sizes[idx] += 1
-            }
-        }
+    for (_, v) in low_point_basins.iter() {
+        *basin_sizes.entry(*v).or_insert(0usize) += 1;
     }
 
-    basin_sizes.sort();
-
-    basin_sizes[basin_sizes.len() - 3..].iter().product()
+    basin_sizes.into_iter()
+        .map(|(_, v)| v)
+        .sorted_by_key(|v| usize::MAX - v)
+        .take(3)
+        .product()
 }
 
 pub fn p2() -> usize {
