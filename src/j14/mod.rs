f2b67fcd8d2065894e391422678b0848a4bbf63f
NicolasVidal-IsCool <nicolas.vidal@iscool-e.Com>
2021-12-14 20:52:33 +0100

    j14 : a little bit less naive, cache cache :)

--- src/j14/mod.rs
+++ src/j14/mod.rs
@@ -4,7 +4,8 @@ use itertools::{Itertools, MinMaxResult};
 
 fn apply_patterns_and_get_result(s: &str, iter: usize) -> usize {
     let mut start_pattern = None;
-    let mut rules = HashMap::new();
+    let mut expand_rules = HashMap::<(char, char), char>::new();
+    let mut counts_rules = HashMap::<((char, char), usize), HashMap<char, usize>>::new();
     for (i, line) in s.lines().enumerate() {
         if i == 0 {
             start_pattern = Some(line);
@@ -13,41 +14,59 @@ fn apply_patterns_and_get_result(s: &str, iter: usize) -> usize {
         if i == 1 {
             continue;
         }
-        let mut rule_splitted = line.split("->");
-        rules.insert(
-            rule_splitted.next().unwrap().trim(),
-            rule_splitted.next().unwrap().trim(),
+        let mut rule_split = line.split("->");
+        let mut map = HashMap::new();
+        let key = rule_split.next().unwrap().trim().chars().collect_tuple::<(char, char)>().unwrap();
+        let v = rule_split.next().unwrap().trim().chars().next().unwrap();
+        map.insert(v, 1usize);
+        expand_rules.insert(
+            key,
+            v,
+        );
+        counts_rules.insert(
+            (key, 0usize),
+            map,
         );
     }
 
-    let mut counts = HashMap::new();
+    for i in 0..(iter-1) {
+        for ((c1, c2), c) in expand_rules.iter() {
+            let mut map = HashMap::new();
 
-    let mut start_pattern = String::from(start_pattern.unwrap());
-    let mut result = String::with_capacity(start_pattern.len() * 2);
-    for _ in 0..iter {
-        for i in 0..(start_pattern.len() - 1) {
-            if let Some(c) = rules.get(&start_pattern[i..i + 2]) {
-                result.push(start_pattern.chars().nth(i).unwrap());
-                result.push(c.chars().next().unwrap());
-            } else {
-                result.push(start_pattern.chars().nth(i).unwrap());
-            }
+            fill_map(&mut counts_rules, i, *c1, *c, &mut map);
+
+            fill_map(&mut counts_rules, i, *c, *c2, &mut map);
+
+            (*map.entry(*c).or_insert(0usize)) += 1;
+
+            counts_rules.insert(((*c1, *c2), i + 1), map);
         }
-        result.push(start_pattern.chars().nth(start_pattern.len() - 1).unwrap());
-        start_pattern = String::from(result);
-        result = String::with_capacity(start_pattern.len() * 2);
     }
 
+
+    let mut map = HashMap::new();
+
+    let start_pattern = start_pattern.unwrap();
     for c in start_pattern.chars() {
-        let entry = counts.entry(c).or_insert(0usize);
-        *entry += 1;
+        (*map.entry(c).or_insert(0usize)) += 1;
     }
 
+    for (c1, c2) in start_pattern.chars().tuple_windows() {
+        for (k, v) in counts_rules.get(&((c1, c2), iter-1)).unwrap() {
+            (*map.entry(*k).or_insert(0usize)) += v;
+        }
+    }
 
-    match counts.into_iter().map(|(_, count)| count).minmax() {
+    match map.into_iter().map(|(_, count)| count).minmax() {
         MinMaxResult::NoElements => { panic!() }
         MinMaxResult::OneElement(_) => { panic!() }
         MinMaxResult::MinMax(min, max) => { max - min }
+    }
+}
+
+fn fill_map(counts_rules: &mut HashMap<((char, char), usize), HashMap<char, usize>>, i: usize, c1: char, c: char, map: &mut HashMap<char, usize>) {
+    for (k, v) in counts_rules.get(&((c1, c), i)).unwrap() {
+        (*map.entry(*k).or_insert(0usize)) += v;
     }
 }
 
@@ -81,6 +100,6 @@ mod j14_tests {
     #[test]
     fn test_p2() {
         assert_eq!(2188189693529, _p2(include_str!("j14_test.txt")));
-        assert_eq!(2188189693529, _p2(include_str!("j14.txt")));
+        assert_eq!(2884513602164, _p2(include_str!("j14.txt")));
     }
 }
