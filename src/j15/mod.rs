a8b20dc231463c4715ea63bc3f4bab1d021aaeab
NicolasVidal-IsCool <nicolas.vidal@iscool-e.Com>
2021-12-16 19:55:12 +0100

    j15 : we now use a BTreeSet in order to be more efficient while searching for minimum left to explored node (-97% time to solve p2 \o/)

--- src/j15/mod.rs
+++ src/j15/mod.rs
@@ -1,11 +1,50 @@
-use std::collections::{HashMap, HashSet};
+use std::cmp::Ordering;
+use std::collections::BTreeSet;
+use std::rc::Rc;
 
-
-#[derive(Clone)]
-struct Node {
+#[derive(Eq, PartialEq, Clone, Debug)]
+struct AStarNode {
+    cost: isize,
     total_cost: isize,
     row: usize,
     col: usize,
+}
+
+impl PartialOrd<Self> for AStarNode {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        match self.total_cost.partial_cmp(&other.total_cost) {
+            None => None,
+            Some(o) => Some(match o {
+                Ordering::Less => o,
+                Ordering::Equal => {
+                    let o = self.row.partial_cmp(&other.row).unwrap();
+                    match o {
+                        Ordering::Less => o,
+                        Ordering::Equal => self.col.partial_cmp(&other.col).unwrap(),
+                        Ordering::Greater => o
+                    }
+                }
+                Ordering::Greater => o
+            })
+        }
+    }
+}
+
+impl Ord for AStarNode {
+    fn cmp(&self, other: &Self) -> Ordering {
+        match self.total_cost.cmp(&other.total_cost) {
+            Ordering::Less => Ordering::Less,
+            Ordering::Equal => {
+                let o = self.row.cmp(&other.row);
+                match o {
+                    Ordering::Less => o,
+                    Ordering::Equal => self.col.cmp(&other.col),
+                    Ordering::Greater => o
+                }
+            }
+            Ordering::Greater => Ordering::Greater
+        }
+    }
 }
 
 fn a_star(s: &str, enlarge_grid: bool) -> usize {
@@ -19,93 +58,112 @@ fn a_star(s: &str, enlarge_grid: bool) -> usize {
         grid.push(row);
     }
 
-    let rows = if enlarge_grid { grid.len() * 5 } else { grid.len() };
-    let cols = if enlarge_grid { grid[0].len() * 5 } else { grid[0].len() };
+    let original_rows = grid.len();
+    let original_cols = grid[0].len();
 
-    let start_node = Node {
-        total_cost: 0,
-        row: 0,
-        col: 0,
-    };
+    let rows = if enlarge_grid { original_rows * 5 } else { original_rows };
+    let cols = if enlarge_grid { original_cols * 5 } else { original_cols };
 
-    let mut closed_nodes = HashSet::with_capacity(rows * cols);
-    let mut known_nodes = HashMap::with_capacity(rows * cols);
-    known_nodes.insert((start_node.row, start_node.col), start_node);
+    let mut nodes_to_explore = BTreeSet::new();
+
+    let mut nodes = Vec::with_capacity(rows);
+    for row in 0..rows {
+        let mut row_vec = Vec::with_capacity(cols);
+        for col in 0..cols {
+            let sub_grid_row = row / original_rows;
+            let sub_grid_col = col / original_cols;
+            let original_value = grid[row % original_rows][col % original_cols] as isize;
+
+            let real_value = ((original_value - 1) + sub_grid_row as isize + sub_grid_col as isize) % 9 + 1;
+
+
+            let node = Rc::new(AStarNode {
+                cost: real_value,
+                total_cost: if row == 0 && col == 0 { 0isize } else { isize::MAX },
+                row,
+                col,
+            });
+            nodes_to_explore.insert(node.clone());
+
+            row_vec.push(node);
+        }
+        nodes.push(row_vec);
+    }
 
     loop {
-        let min_node = known_nodes
-            .iter_mut()
-            .min_by_key(|(_, n)| n.total_cost
-                        // heuristic seems to harm performances
-                        // + (n.row as isize - rows as isize + 1).abs()
-                        // + (n.col as isize - cols as isize + 1).abs()
-            ).unwrap().1;
+        let min_node = nodes_to_explore.iter().next().unwrap().clone();
 
-        let min_node = min_node.clone();
-
-        known_nodes.remove(&(min_node.row, min_node.col));
-        closed_nodes.insert((min_node.row, min_node.col));
+        let min_node = nodes_to_explore.take(&min_node).unwrap();
 
         if min_node.col == cols - 1 && min_node.row == rows - 1 {
             return min_node.total_cost as usize;
         }
 
         if min_node.col > 0 {
-            update_neighbour(&mut grid,
-                             &closed_nodes,
-                             &mut known_nodes, &min_node,
+            update_neighbour(&mut nodes_to_explore,
+                             &mut nodes,
+                             &min_node,
                              min_node.row,
                              min_node.col - 1);
         }
         if min_node.row > 0 {
-            update_neighbour(&mut grid,
-                             &closed_nodes,
-                             &mut known_nodes, &min_node,
-                             min_node.row - 1,
-                             min_node.col);
+            update_neighbour(
+                &mut nodes_to_explore,
+                &mut nodes,
+                &min_node,
+                min_node.row - 1,
+                min_node.col);
         }
         if min_node.col < cols - 1 {
-            update_neighbour(&mut grid,
-                             &closed_nodes,
-                             &mut known_nodes, &min_node,
-                             min_node.row,
-                             min_node.col + 1);
+            update_neighbour(
+                &mut nodes_to_explore,
+                &mut nodes,
+                &min_node,
+                min_node.row,
+                min_node.col + 1);
         }
         if min_node.row < rows - 1 {
-            update_neighbour(&mut grid,
-                             &closed_nodes,
-                             &mut known_nodes, &min_node,
-                             min_node.row + 1,
-                             min_node.col);
+            update_neighbour(
+                &mut nodes_to_explore,
+                &mut nodes,
+                &min_node,
+                min_node.row + 1,
+                min_node.col);
         }
     }
 }
 
-fn update_neighbour(grid: &mut Vec<Vec<u8>>,
-                    closed_nodes: &HashSet<(usize, usize)>,
-                    known_nodes: &mut HashMap<(usize, usize), Node>, min_node: &Node,
+fn update_neighbour(nodes_to_explore: &mut BTreeSet<Rc<AStarNode>>,
+                    nodes: &mut Vec<Vec<Rc<AStarNode>>>,
+                    min_node: &AStarNode,
                     n_row: usize, n_col: usize) {
-    if closed_nodes.contains(&(n_row, n_col)) {
-        return;
-    }
-    let neighbour = known_nodes.entry((n_row, n_col))
-        .or_insert(Node {
-            total_cost: isize::MAX,
+    let neighbour = &nodes[n_row][n_col];
+
+    if neighbour.total_cost - neighbour.cost > min_node.total_cost {
+        nodes_to_explore.remove(neighbour);
+        let updated_node = Rc::new(AStarNode {
+            total_cost: min_node.total_cost + neighbour.cost,
             row: n_row,
             col: n_col,
+            cost: neighbour.cost,
         });
-    let real_rows = grid.len();
-    let real_cols = grid[0].len();
 
-    let sub_grid_row = n_row / real_rows;
-    let sub_grid_col = n_col / real_cols;
-    let original_value = grid[n_row % real_rows][n_col % real_cols] as isize;
-
-    let real_value = ((original_value - 1) + sub_grid_row as isize + sub_grid_col as isize) % 9 + 1;
-
-    if neighbour.total_cost - real_value as isize > min_node.total_cost {
-        neighbour.total_cost = min_node.total_cost + real_value as isize;
+        nodes_to_explore.insert(updated_node.clone());
+        nodes[n_row][n_col] = updated_node;
     }
+
+    // let real_rows = grid.len();
+    // let real_cols = grid[0].len();
+    //
+    // let sub_grid_row = n_row / real_rows;
+    // let sub_grid_col = n_col / real_cols;
+    // let original_value = grid[n_row % real_rows][n_col % real_cols] as isize;
+    //
+    // let real_value = ((original_value - 1) + sub_grid_row as isize + sub_grid_col as isize) % 9 + 1;
+    //
+    // if neighbour.total_cost - real_value as isize > min_node.total_cost {
+    //     neighbour.total_cost = min_node.total_cost + real_value as isize;
+    // }
 }
 
 
